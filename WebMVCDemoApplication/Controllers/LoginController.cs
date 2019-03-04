fc69fccc1f9623dbf3fb9e5d751500e75dc23cda
embaobao <1132067567@qq.com>
2019-03-04 23:06:39 +0800

    控制器和视图的相关基础介绍 ，创建基本的控制器 和相关视图，添加了学习控制器简单调用视图。

---
+++ WebMVCDemoApplication/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVCDemoApplication.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+
+            ViewBag.Message = "登录页面";
+            //展现数据到视图
+            return View();
+        }
+        // GET: Access
+        public ActionResult Access(string name,string pw)
+        {
+            ViewBag.Name = Server.HtmlEncode(name);
+            ViewBag.PW = Server.HtmlEncode(pw);
+            return View("Showlogin");
+        }
+        // GET: Fail
+        public string Fail()
+        {
+            return "登录失败 请返回重新登录";
+        }
+
+    }
+}
